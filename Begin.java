441afe755701359e6014d843338ebf072c37f3ca
Jarrod Rotolo <jrotol5@lsu.edu>
2014-09-26 17:19:20 -0500

    Added print method to Begin Node

--- Begin.java
+++ Begin.java
@@ -8,5 +8,10 @@ class Begin extends Special {
     // TODO: Add an appropriate constructor.
 
     void print(Node t, int n, boolean p) {
+    	System.out.println("(begin");
+    	Node cdr = t.getCdr();
+    	if (cdr != null)
+    		cdr.print(n, p);
+    	System.out.println("");
     }
 }
