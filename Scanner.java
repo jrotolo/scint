9a437a52d058c90997ec97e763255fb5934b5c06
Caleb <cleger35@gmail.com>
2014-09-30 11:28:25 -0500

    Fixed indentation in scanner.java

--- Scanner.java
+++ Scanner.java
@@ -5,163 +5,164 @@ import java.util.*;
 import java.lang.StringBuilder;
 
 class Scanner {
-  private PushbackInputStream in;
-  private byte[] buf = new byte[1000];
+	private PushbackInputStream in;
+	private byte[] buf = new byte[1000];
 
-  public Scanner(InputStream i) { in = new PushbackInputStream(i); }
+	public Scanner(InputStream i) { in = new PushbackInputStream(i); }
 
-  public Token getNextToken() {
-    int bite = -1;
+	public Token getNextToken() {
+		int bite = -1;
 
-    // It would be more efficient if we'd maintain our own input buffer
-    // and read characters out of that buffer, but reading individual
-    // characters from the input stream is easier.
-    try {
-      bite = in.read();
-    } catch (IOException e) {
-      System.err.println("We fail: " + e.getMessage());
-    }
-
-    if (bite == -1)
-      return null;
-
-   char ch = (char) bite;
-
-  	if (ch == ' ' || ch == '\n'){
-  		return getNextToken();
-  	} else if (ch == ';') {
-  		do {
-  			try {
-  				bite = in.read();
-  			} catch (IOException e) {
-  				System.err.println("We fail: " + e.getMessage());
-  			}
-  			ch = (char) bite;
-  		} while (!(ch == '\n'));
-  		return getNextToken();
-  	}
-
-    // Special characters
-    if (ch == '\'')
-      return new Token(Token.QUOTE);
-    else if (ch == '(')
-      return new Token(Token.LPAREN);
-    else if (ch == ')')
-      return new Token(Token.RPAREN);
-    else if (ch == '.')
-      // We ignore the special identifier `...'.
-      return new Token(Token.DOT);
-
-    // Boolean constants
-    else if (ch == '#') {
-      try {
+		// It would be more efficient if we'd maintain our own input buffer
+		// and read characters out of that buffer, but reading individual
+		// characters from the input stream is easier.
+		try {
 			bite = in.read();
-      } catch (IOException e) {
+		} catch (IOException e) {
 			System.err.println("We fail: " + e.getMessage());
-      }
+		}
 
-      if (bite == -1) {
-	System.err.println("Unexpected EOF following #");
-	return null;
-      }
-      ch = (char) bite;
-      if (ch == 't')
-	return new Token(Token.TRUE);
-      else if (ch == 'f')
-	return new Token(Token.FALSE);
-      else {
-	System.err.println("Illegal character '" + ch + "' following #");
-	return getNextToken();
-      }
-    }
+		if (bite == -1)
+			return null;
 
-    // String constants
-    else if (ch == '"') {
-      StringBuilder str = new StringBuilder();
+		char ch = (char) bite;
 
-      try {
-        bite = in.read();
-      } catch (IOException e) {
-        System.err.println("We fail: " + e.getMessage());
-      }
-      ch = (char) bite;
+		if (ch == ' ' || ch == '\n') {
+			return getNextToken();
+		} 
+		else if (ch == ';') {
+			do {
+				try {
+					bite = in.read();
+				} catch (IOException e) {
+					System.err.println("We fail: " + e.getMessage());
+				  }
+				ch = (char) bite;
+			} while (!(ch == '\n'));
+				return getNextToken();
+		}
 
-  		while (!(ch == '"')) {
-        str.append(ch);
-  			try {
-  				bite = in.read();
-  			} catch (IOException e) {
-  				System.err.println("We fail: " + e.getMessage());
-  			}
-        ch = (char) bite;
-  		}
+		// Special characters
+		if (ch == '\'')
+			return new Token(Token.QUOTE);
+		else if (ch == '(')
+			return new Token(Token.LPAREN);
+		else if (ch == ')')
+			return new Token(Token.RPAREN);
+		else if (ch == '.')
+			// We ignore the special identifier `...'.
+			return new Token(Token.DOT);
 
-      return new StrToken(str.toString());
-    }
+		// Boolean constants
+		else if (ch == '#') {
+			try {
+				bite = in.read();
+			} catch (IOException e) {
+				System.err.println("We fail: " + e.getMessage());
+		  	  }
 
-    // Integer constants
-    else if (ch >= '0' && ch <= '9') {
-      int i = 0;
-  		Stack<Integer> intStack = new Stack();
+			if (bite == -1) {
+				System.err.println("Unexpected EOF following #");
+				return null;
+			}
+			ch = (char) bite;
+			if (ch == 't')
+				return new Token(Token.TRUE);
+			else if (ch == 'f')
+				return new Token(Token.FALSE);
+			else {
+				System.err.println("Illegal character '" + ch + "' following #");
+				return getNextToken();
+			}
+		}
 
-  		do {
-  			i = ch - '0';
-  			intStack.push(new Integer(i));
-  			try {
-  				bite = in.read();
-  			} catch (IOException e) {
-  				System.err.println("We fail: " + e.getMessage());
-  			}
-  			ch = (char) bite;
-  		} while ((ch >= '0' && ch <= '9'));
+		// String constants
+		else if (ch == '"') {
+			StringBuilder str = new StringBuilder();
 
-  		int intValue = 0;
-  		int j = 0;
+			try {
+				bite = in.read();
+		  	} catch (IOException e) {
+				System.err.println("We fail: " + e.getMessage());
+		  	  }
+			ch = (char) bite;
 
-  		while (!intStack.empty()) {
-  			intValue += intStack.pop() * Math.pow(10, j++);
-  		}
+			while (!(ch == '"')) {
+				str.append(ch);
+				try {
+					bite = in.read();
+				} catch (IOException e) {
+					System.err.println("We fail: " + e.getMessage());
+				  }
+				ch = (char) bite;
+			}
 
-  		try {
-  			in.unread(ch);
-  		} catch (IOException e) {
-  			System.err.println("We fail: " + e.getMessage());
-  		}
+			return new StrToken(str.toString());
+		}
 
-      return new IntToken(intValue);
-    }
+		// Integer constants
+		else if (ch >= '0' && ch <= '9') {
+			int i = 0;
+			Stack<Integer> intStack = new Stack();
 
-    // Identifiers
-    else if (ch >= 'A' && ch <= 'Z') {
-      StringBuilder str = new StringBuilder();
-      str.append(ch);
-  		do {
-  			try {
-  				bite = in.read();
-  			} catch (IOException e) {
-  				System.err.println("We fail: " + e.getMessage());
-  			}
-  			ch = (char) bite;
-        str.append(ch);
-  		} while (ch >= 'a' && ch <= 'z');
-      // put the character after the identifier back into the input
-      try {
-        in.unread(ch);
-      } catch (IOException e) {
-        System.err.println("We fail: " + e.getMessage());
-      }
+			do {
+				i = ch - '0';
+				intStack.push(new Integer(i));
+				try {
+					bite = in.read();
+				} catch (IOException e) {
+					System.err.println("We fail: " + e.getMessage());
+				  }
+				ch = (char) bite;
+			} while ((ch >= '0' && ch <= '9'));
 
-      return new IdentToken(str.toString());
-    }
+			int intValue = 0;
+			int j = 0;
 
-    else if (ch == '+' || ch == '-' || ch == '/' || ch == '*'){
-      return new IdentToken(Character.toString(ch));
-    }
+			while (!intStack.empty()) {
+				intValue += intStack.pop() * Math.pow(10, j++);
+			}
 
-    // Illegal character
-    else {
-      System.err.println("Illegal input character " + (char) ch);
-      return getNextToken();
-    }
-  };
+			try {
+				in.unread(ch);
+			} catch (IOException e) {
+				System.err.println("We fail: " + e.getMessage());
+			  }
+
+			return new IntToken(intValue);
+		}
+
+		// Identifiers
+		else if (ch >= 'A' && ch <= 'Z') {
+			StringBuilder str = new StringBuilder();
+			str.append(ch);
+			do {
+				try {
+					bite = in.read();
+				} catch (IOException e) {
+					System.err.println("We fail: " + e.getMessage());
+				  }
+				ch = (char) bite;
+				str.append(ch);
+			} while (ch >= 'a' && ch <= 'z');
+		  // put the character after the identifier back into the input
+		  try {
+			in.unread(ch);
+		  } catch (IOException e) {
+				System.err.println("We fail: " + e.getMessage());
+		    }
+
+			return new IdentToken(str.toString());
+		}
+
+		else if (ch == '+' || ch == '-' || ch == '/' || ch == '*') {
+			return new IdentToken(Character.toString(ch));
+		}
+
+		// Illegal character
+		else {
+			System.err.println("Illegal input character " + (char) ch);
+			return getNextToken();
+		}
+	};
 }
