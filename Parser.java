e56e491e4155cc05ad89207439dff3fad48532d0
Caleb <cleger35@gmail.com>
2014-09-30 11:34:20 -0500

    Fixed the indentation in Parser.java

--- Parser.java
+++ Parser.java
@@ -34,63 +34,63 @@
 // or an RPAREN) and attempts to continue parsing with the next token.
 
 class Parser {
-  private Scanner scanner;
-  private Node nilNode;
-  private Node trueNode;
-  private Node falseNode;
+ 	private Scanner scanner;
+ 	private Node nilNode;
+ 	private Node trueNode;
+ 	private Node falseNode;
 
-  public Parser(Scanner s) { scanner = s; }
+ 	public Parser(Scanner s) { scanner = s; }
   
-  public Node parseExp() {
-    return parseExp(scanner.getNextToken());
-  }
+ 	public Node parseExp() {
+		return parseExp(scanner.getNextToken());
+	}
 
-  public Node parseExp(Token inputToken) {
-    // TODO: Figure out how to deal with TokenType QUOTE and DOT
-    if (inputToken.getType() == Token.LPAREN)
-      return parseRest();
-    else if (inputToken.getType() == Token.TRUE)
-      return this.getTrueNode();
-    else if (inputToken.getType() == Token.FALSE)
-      return this.getFalseNode();
-    else if (inputToken.getType() == Token.INT)
-      return new IntLit(inputToken.getIntVal());
-    else if (inputToken.getType() == Token.STRING)
-      return new StrLit(inputToken.getStrVal());
-    else if (inputToken.getType() == Token.IDENT)
-      return new Ident(inputToken.getName());
-    return null;
-  }
+ 	public Node parseExp(Token inputToken) {
+		// TODO: Figure out how to deal with TokenType QUOTE and DOT
+		if (inputToken.getType() == Token.LPAREN)
+	 		return parseRest();
+		else if (inputToken.getType() == Token.TRUE)
+	 		return this.getTrueNode();
+		else if (inputToken.getType() == Token.FALSE)
+	 		return this.getFalseNode();
+		else if (inputToken.getType() == Token.INT)
+	 		return new IntLit(inputToken.getIntVal());
+		else if (inputToken.getType() == Token.STRING)
+	 		return new StrLit(inputToken.getStrVal());
+		else if (inputToken.getType() == Token.IDENT)
+	 		return new Ident(inputToken.getName());
+		return null;
+ 	}
   
-  protected Node parseRest() {
-    return parseRest(scanner.getNextToken());
-  }
+	protected Node parseRest() {
+		return parseRest(scanner.getNextToken());
+  	}
 
-  protected Node parseRest(Token inputToken) {
-    // TODO: write code for parsing DOT and QUOTE
-    if (inputToken.getType() == Token.RPAREN)
-      return getNilNode();
-    else
-      return new Cons(parseExp(inputToken), parseRest()); 
-  }
+	protected Node parseRest(Token inputToken) {
+		// TODO: write code for parsing DOT and QUOTE
+		if (inputToken.getType() == Token.RPAREN)
+	  		return getNilNode();
+		else
+	  		return new Cons(parseExp(inputToken), parseRest()); 
+  	}
   
 
-  // Methods for singleton node objects
-  public Node getNilNode() {
-    if (nilNode == null)
-      nilNode = new Nil();
-    return nilNode;
-  }
+	// Methods for singleton node objects
+	public Node getNilNode() {
+		if (nilNode == null)
+	  		nilNode = new Nil();
+		return nilNode;
+  	}
 
-  public Node getTrueNode() {
-    if (trueNode == null)
-      trueNode = new BooleanLit(true);
-    return trueNode;
-  }
+	public Node getTrueNode() {
+		if (trueNode == null)
+	  		trueNode = new BooleanLit(true);
+		return trueNode;
+  	}
 
-  public Node getFalseNode() {
-    if (falseNode == null)
-      falseNode = new BooleanLit(false);
-    return falseNode;
-  }
+	public Node getFalseNode() {
+		if (falseNode == null)
+	  		falseNode = new BooleanLit(false);
+		return falseNode;
+	}
 };
