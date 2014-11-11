830fe22290022d43c30c3f6f92151dfbbd447e57
Jarrod <jrotol5@lsu.edu>
2014-11-11 14:05:15 -0600

    Updated run script to work on windows and linux

--- run.sh
+++ run.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
+platform='unknown'
+unamestr=`uname`
 
-echo "Compiling program..."
-DIR="bin"
-if [ ! -d "$DIR" ]; then
-    mkdir bin
+if [[ "$unamestr" == 'MINGW32_NT-6.1' ]]; then
+	platform='windows'
+elif [[ "$unamestr" == 'Linux' ]]; then
+	platform='linux'
 fi
-javac -d bin -cp lib/spp.jar src/*.java
 
-echo "Running program..."
-java -cp bin:lib/spp.jar Main
+if [[ "$platform" == 'unknown' ]]; then
+	echo "Error can't detect os type"
+	exit
+elif [[ "$platform" == 'linux' ]]; then
+	printf "Compiling program...\n"
+	DIR="bin"
+	if [ ! -d "$DIR" ]; then
+	    mkdir bin
+	fi
+	javac -d bin -cp lib/spp.jar src/*.java
+
+	printf "Running program...\n"
+	java -cp bin:lib/spp.jar Main
+elif [[ "$platform" == 'windows' ]]; then
+	printf "Compiling program...\n"
+	DIR="bin"
+	if [[ ! -d "$DIR" ]]; then
+		mkdir bin
+	fi
+	javac -d bin -cp lib/spp.jar src/*.java
+
+	printf "Running program...\n"
+	cd src
+	java -cp ../bin:../lib Main
+fi
