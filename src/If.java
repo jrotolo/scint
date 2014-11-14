1fde0bc75bf2e02230b0705ff20b32bfec2c1e58
Jarrod <jrotol5@lsu.edu>
2014-11-13 23:24:39 -0600

    If eval working

--- src/If.java
+++ src/If.java
@@ -7,5 +7,12 @@ class If extends Special {
     	  Printer.printIf(t, n, p);
     }
 
-    public Node eval(Node t, Environment env) { return null; }
+    public Node eval(Node t, Environment env) { 
+    	Node cond = t.getCdr().getCar();
+    	Node ifClause = t.getCdr().getCdr().getCar();
+    	Node elseClause = t.getCdr().getCdr().getCdr().getCar();
+    	Node value = null;
+
+    	return (cond.eval(env).getBoolean()) ? ifClause.eval(env) : elseClause.eval(env);
+    }
 }
